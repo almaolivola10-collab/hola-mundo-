64fe791a529019428e72d2e2f04bb66825e1fed5
almaolivola10-collab <almaolivola10@gmail.com>
2026-08-13 15:59:13 -0300

    Add files via upload

---
+++ HOLAMUNDO/src/main.cpp
@@ -0,0 +1,29 @@
+/* 
+   ====================================================================
+   Materia: Laboratorio de Programación (LPR)
+   E.E.S.T. N° 1 "Eduardo Ader" — Vicente López
+   Curso: 5° año
+   Profesor: Prof. York
+   Archivo: main.cpp
+   Objetivo: Compilar y ejecutar nuestro primer programa de consola en C++.
+   ====================================================================
+*/
+
+#include <iostream> // Es la Biblioteca estandar para operaciones de entrada y salida (cout/cin)
+
+using namespace std; // Evita tener que anteponer 'std::' antes de cada comando de consola o terminal
+
+int main() {
+    // ====================================================================
+    // REGLA OBLIGATORIA (EVITA EL COPIADO):
+    // Cada estudiante debe modificar la salida de pantalla agregando su 
+    // Nombre y Apellido reales antes de compilar y realizar la entrega de la actividad 2.
+    // ====================================================================
+    
+    cout << "=====================================================" << endl;
+    cout << "  Hola Mundo! Bienvenido al Taller de LPR 2026.      " << endl;
+    cout << "  Estudiante: [Alma Olivola]   " << endl;
+    cout << "=====================================================" << endl;
+
+    return 0; // Le indica al sistema operativo Windows 11 que el programa finalizo con exito (codigo 0)
+}
